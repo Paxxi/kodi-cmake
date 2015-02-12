biopendent/xbmc-cmake

add support for audio decoder addons

xbmc/cores/paplayer/CodecFactory.cpp
@@ -41,9 +41,25 @@
 #include "DVDPlayerCodec.h"
 #include "PCMCodec.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 
 ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
 {
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->GetExtensions().find("."+strFileType) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
   if (strFileType.Equals("mp3") || strFileType.Equals("mp2"))
     return new MP3Codec();
   else if (strFileType.Equals("pcm") || strFileType.Equals("l16"))
@@ -116,6 +132,19 @@ ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
 ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdString& strContent, unsigned int filecache)
 {
   CURL urlFile(strFile);
+
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->GetMimetypes().find(strContent) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
   if( strContent.Equals("audio/mpeg")
   ||  strContent.Equals("audio/mpeg3")
   ||  strContent.Equals("audio/mp3") )

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -49,7 +49,10 @@
 #include "settings/AdvancedSettings.h"
 #include "FileItem.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
 
+using namespace ADDON;
 using namespace XFILE;
 using namespace PLAYLIST;
 using namespace std;
@@ -81,6 +84,23 @@ IFileDirectory* CFileDirectoryFactory::Create(const CStdString& strPath, CFileIt
 
     delete pDir;
     return NULL;
+  }
+
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (!strExtension.empty() && dec->HasTracks() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      if (result->ContainsFiles(strPath))
+        return result;
+      delete result;
+      return NULL;
+    }
   }
   if (strExtension.Equals(".nsf") && CFile::Exists(strPath))
   {

Paxxi/kodi-cmake

xbmc/cores/paplayer/CodecFactory.cpp
@@ -36,11 +36,27 @@
 #include "URL.h"
 #include "DVDPlayerCodec.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 
 ICodec* CodecFactory::CreateCodec(const std::string& strFileType)
 {
   std::string fileType = strFileType;
   StringUtils::ToLower(fileType);
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->GetExtensions().find("."+fileType) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
   if (fileType == "mp3" || fileType == "mp2")
     return new DVDPlayerCodec();
   else if (fileType == "pcm" || fileType == "l16")
@@ -121,6 +137,21 @@ ICodec* CodecFactory::CreateCodecDemux(const std::string& strFile, const std::st
   CURL urlFile(strFile);
   std::string content = strContent;
   StringUtils::ToLower(content);
+  if (!content.empty())
+  {
+    VECADDONS codecs;
+    CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+    for (size_t i=0;i<codecs.size();++i)
+    {
+      boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+      if (dec->GetMimetypes().find(content) != std::string::npos)
+      {
+        CAudioDecoder* result = new CAudioDecoder(*dec);
+        static_cast<AudioDecoderDll&>(*result).Create();
+        return result;
+      }
+    }
+  }
   if( content == "audio/mpeg"
   ||  content == "audio/mpeg3"
   ||  content == "audio/mp3" )

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -50,7 +50,10 @@
 #include "FileItem.h"
 #include "utils/StringUtils.h"
 #include "URL.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
 
+using namespace ADDON;
 using namespace XFILE;
 using namespace PLAYLIST;
 using namespace std;
@@ -79,6 +82,22 @@ IFileDirectory* CFileDirectoryFactory::Create(const CURL& url, CFileItem* pItem,
 
     delete pDir;
     return NULL;
+  }
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (!strExtension.empty() && dec->HasTracks() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      if (result->ContainsFiles(url))
+        return result;
+      delete result;
+      return NULL;
+    }
   }
   if (url.IsFileType("nsf") && CFile::Exists(url))
   {